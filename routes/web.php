89255760b9ffc5a30b4d131b86ce0d1b36c7fe4d
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-04 16:16:26 +0200

    FEAT laravel + route to homepage created

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+// Route::get('/', function () {
+//     return view('contacts');
+// });
+
+// Route::get('/', function () {
+//     return view('about');
+// });
+
+// Route::get('/', function () {
+//     return view('services');
+// });
